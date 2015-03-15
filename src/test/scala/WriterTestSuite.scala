081934b001423c93d8ef6cd6f6d94b272c92772a
Alexander Ryblov <ryblov.a.v@gmail.com>
2015-03-15 08:53:10 +0300

    add test

--- src/test/scala/WriterTestSuite.scala
+++ src/test/scala/WriterTestSuite.scala
@@ -1,3 +1,24 @@
-class WriterTestSuite {
+import HibernateEntityGenerator.writer.EntityWriter
+import org.scalatest._
+
+class WriterTestSuite extends FunSpec with ShouldMatchers {
+
+  describe("write entity") {
+
+    val pathFolder = EntityWriter.buildPathFolder("HibernateEntityGenerator.builders")
+
+    it("package to folder name") {
+      EntityWriter.packageToFolderName("HibernateEntityGenerator.builders") should be ("HibernateEntityGenerator/builders")
+    }
+
+    it("build path folder") {
+      info(s"pathFolder = $pathFolder")
+      pathFolder should be ("src/main/resources/gen/HibernateEntityGenerator/builders")
+    }
+
+    it("build full path") {
+      EntityWriter.buildFullPath("org.loader.pojo.sa", "CI_SA_CHAR") should be ("src/main/resources/gen/org/loader/pojo/sa/SaCharEntity.java")
+    }
+  }
 
 }
