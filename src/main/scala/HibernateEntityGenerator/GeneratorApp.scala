88763d27901896f63210f6bf6d43b0173e7b45ba
Alexander Ryblov <ryblov.a.v@gmail.com>
2015-03-20 11:00:57 +0300

    changed app class

--- src/main/scala/HibernateEntityGenerator/GeneratorApp.scala
+++ src/main/scala/HibernateEntityGenerator/GeneratorApp.scala
@@ -9,14 +9,10 @@ object GeneratorApp extends App with Logging {
 
   val packageName = "org.loader.new.pojo"
 
-  val tables = DBSchemeReader.readScheme.filter((t) => t.name == "CI_SA_CHAR")
-
+  val tables = DBSchemeReader.readScheme//.filter((t) => t.name == "CI_SA_CHAR")
 
   tables.foreach((t) => EntityWriter.writeEntity(tableName = t.name,
     packageName = packageName,
     entityStr = EntityBuilder.build(t, packageName)))
-
-  //  EntityWriter.writeEntity()
-
 
 }
